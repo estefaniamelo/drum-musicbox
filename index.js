856eb48ee00757099593eb06ce4fbd1c4a0755a1
estefaniamelo <melojoa@sheridancollege.ca>
2022-06-06 16:38:36 -0400

    In the JS file, logic was added that will allow the user to hear the sound when they click the corresponding color

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 window.addEventListener('load', () => {
     const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelector('.visual');
+    const colors = [
+    "#60D394",
+    "#D36060",
+    "#C060D3",
+    "#D3D160",
+    "#606BD3",
+    "#60C2D3"
+    ];
+
+    // lets get going with the sound here 
+    pads.forEach((pad, index) => {
+        pad.addEventListener('click', function(){
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+        });
+    });
 });
+
