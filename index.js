29c0a4e203790b78c08150313dce6308afb1d714
estefaniamelo <melojoa@sheridancollege.ca>
2022-06-06 16:46:55 -0400

    Added a function that removes the bubble once the sound has been played to improve performance.

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ window.addEventListener('load', () => {
     const pads = document.querySelectorAll(".pads div");
     const visual = document.querySelector('.visual');
     const colors = [
-    "#60D394",
-    "#D36060",
-    "#C060D3",
-    "#D3D160",
-    "#606BD3",
-    "#60C2D3"
+        "#60D394",
+        "#D36060",
+        "#C060D3",
+        "#D3D160",
+        "#606BD3",
+        "#60C2D3"
     ];
 
     // lets get going with the sound here 
@@ -16,7 +16,22 @@ window.addEventListener('load', () => {
         pad.addEventListener('click', function(){
             sounds[index].currentTime = 0;
             sounds[index].play();
+
+            createBubbles(index);
         });
     });
+
+    // create a function that makes bubbles
+    const createBubbles = (index) => {
+        const bubble = document.createElement("div");
+        visual.appendChild(bubble);
+
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = 'jump 1s ease';
+        bubble.addEventListener('animationend', function(){
+            visual.removeChild(this);
+        });
+    }
+
 });
 
